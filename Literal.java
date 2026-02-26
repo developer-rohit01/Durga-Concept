eec718013d1c84938cf5d08a1f0fc09673e9baa2
Rohit <rr5450534@gmail.com>
2026-02-26 21:08:44 +0530

    Add literal lecture 04

---
+++ Literal.java
@@ -0,0 +1,22 @@
+
+public class Literal {
+  public static void main(String[] args) {
+    int a =200; //Decimal form of Integral Literal allowed = 0 t0 9
+    int b =010;//Octal form of Integral Literal allowed = 0 to 7
+    int c =0x200;//HexaDecimal form of Integral Literal , should be prefixed with OX
+    int e=0XFace;//HexaDecimal form of Integral Literal , should be prefixed with OX
+
+    int d = a+b+c;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+
+
+    int String =10;
+    System.out.println(String);
+
+    
+  }
+}
