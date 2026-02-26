6d57e2c1ebe3649f8aa2ae3eb9fb2fef15db8a3b
Rohit <rr5450534@gmail.com>
2026-02-26 21:06:24 +0530

    Add error Marking in datatype

---
+++ Lecture05.java
@@ -0,0 +1,15 @@
+
+
+public class Lecture05 {
+  public static void main(String[] args) {
+     int a =true; //incompatible types: boolean cannot be converted to int
+     int b=2147483648; //integer no too large 
+     int c =2147483647L; //incompatible types: possible lossy conversion from long to int
+     System.out.println(a);
+     System.out.println(b);
+     System.out.println(c);
+
+  }
+  
+  
+}
