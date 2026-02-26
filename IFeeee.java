2946eb4d5704a5221128206d78e344e2e9274800
Rohit <rr5450534@gmail.com>
2026-02-26 21:04:37 +0530

    Add IfElse return boolean in condition

---
+++ IFeeee.java
@@ -0,0 +1,10 @@
+class Ifeeee {
+  public static void main(String[] args) {
+    int b=0;
+     if(b==0){
+      System.out.println("hello");
+     }else {
+      System.out.println("not hello");
+     }
+  }
+}
