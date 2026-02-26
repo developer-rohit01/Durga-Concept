5c136c71b3426ed873c8460eb31ca731133f7f01
Rohit <rr5450534@gmail.com>
2026-02-26 21:07:46 +0530

    Add Lecture 06 2 byte char in java

---
+++ Lecture06.java
@@ -0,0 +1,25 @@
+
+
+public class Lecture06 {
+  public static void main(String[] args) {
+    float a = 10.0f/3;//for low precision upto 6-7 digits after decimal point
+    
+    double b = 10.0f/3;//for much precision upto 15-16 digits after decimal point
+
+    char ch = 'ञ';
+
+    System.out.println(a);
+    System.out.println(b);
+
+    // int c =0;
+    // if(c==0)//incompatible types: int cannot be converted to boolean
+    //   {  
+    //   System.out.println("Hello");
+    // }
+
+    System.out.println(ch);
+    
+    }
+  
+}
+ 
