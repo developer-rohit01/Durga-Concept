9577ef928091f33191e1c3f89ef71873eaa054aa
Rohit <rr5450534@gmail.com>
2026-04-05 22:14:16 +0530

    Update the Lecture05

--- Lecture05.java
+++ Lecture05.java
@@ -4,7 +4,7 @@ public class Lecture05 {
   public static void main(String[] args) {
      int a =true; //incompatible types: boolean cannot be converted to int
      int b=2147483648; //integer no too large 
-     int c =2147483647L; //incompatible types: possible lossy conversion from long to int
+     int c =21474836477L; //incompatible types: possible lossy conversion from long to int
      System.out.println(a);
      System.out.println(b);
      System.out.println(c);
